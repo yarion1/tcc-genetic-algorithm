1b1a2f0931d8a4188aa251045cb5559da7361853
Luanatvrs <luanalmtavaress@gmail.com>
2024-05-19 13:34:11 -0300

    refactor:request evento

--- app/Http/Requests/Horario/EventoRequest.php
+++ app/Http/Requests/Horario/EventoRequest.php
@@ -12,10 +12,28 @@ class EventoRequest extends FormRequest
         return true;
     }
 
+    public function messages()
+    {
+        return  [
+            'room_id' => 'O campo Local é obrigatório.',
+            'course_id' => 'O campo Disciplina é obrigatório.',
+            'professor_id' => 'O campo Professor é obrigatório.',
+            'startTime' => 'A data de início é obrigatória',
+            'endTime' => 'A data fim é obrigatória.'
+        ];
+    }
+
     public function rules(): array
     {
         return [
-            //
+            'course_id' => 'required|integer',
+            'horario_id' => 'required|integer',
+            'startTime' => 'required|string',
+            'title' => 'required|string',
+            'endTime' => 'required|string',
+            'daysOfWeek' => 'required|array',
+            'room_id' => 'required|integer',
+            'professor_id' => 'required|integer',
         ];
     }
 }
