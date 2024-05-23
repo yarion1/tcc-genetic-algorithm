2b1a4a52f43382781104b5acadddb660168e93b4
Pablo Henrique <74999976+yarion1@users.noreply.github.com>
2024-05-23 18:41:30 -0300

    Update DaysTableSeeder.php

--- database/seeders/DaysTableSeeder.php
+++ database/seeders/DaysTableSeeder.php
@@ -14,32 +14,46 @@ class DaysTableSeeder extends Seeder
     {
         $days = [
             [
+                'id' => 1,
                 'name' => 'Monday',
-                'short_name' => 'Mon'
+                'short_name' => 'Mon',
+                'daysOfWeek' => 1,
             ],
             [
+                'id' => 2,
                 'name' => 'Tuesday',
-                'short_name' => 'Tue'
+                'short_name' => 'Tue',
+                'daysOfWeek' => 2,
             ],
             [
+                'id' => 3,
                 'name' => 'Wednesday',
-                'short_name' => 'Wed'
+                'short_name' => 'Wed',
+                'daysOfWeek' => 3,
             ],
             [
+                'id' => 4,
                 'name' => 'Thursday',
-                'short_name' => 'Thur'
+                'short_name' => 'Thur',
+                'daysOfWeek' => 4,
             ],
             [
+                'id' => 5,
                 'name' => 'Friday',
-                'short_name' => 'Fri'
+                'short_name' => 'Fri',
+                'daysOfWeek' => 5,
             ],
             [
+                'id' => 6,
                 'name' => 'Saturday',
-                'short_name' => 'Sat'
+                'short_name' => 'Sat',
+                'daysOfWeek' => 6,
             ],
             [
+                'id' => 7,
                 'name' => 'Sunday',
-                'short_name' => 'Sun'
+                'short_name' => 'Sun',
+                'daysOfWeek' => 8,
             ]
         ];
 
