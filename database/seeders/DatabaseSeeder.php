603cffe745db559a6c67e7a430fa65a035e87122
Luanatvrs <luanalmtavaress@gmail.com>
2024-05-26 17:11:24 -0300

    feat:seed menu perfil

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,7 +35,8 @@ class DatabaseSeeder extends Seeder
             // DiaSemanaSeeder::class,
              //TipoRestricaoSeeder::class,
             // RestricaoSeed::class,
-            MenuSeeder::class
+            MenuSeeder::class,
+            MenuPerfilSeeder::class
         ]);
         
     }
